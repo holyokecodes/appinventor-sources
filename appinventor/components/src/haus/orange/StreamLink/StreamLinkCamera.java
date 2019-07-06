3c6379a2301978609e0623ea150b946be1175f3e
Jacob Bashista <tgb.bashista@gmail.com>
2019-07-06 16:36:19 -0400

    Reworked StreamLinkCamera to minimize error, something wrong with camera configuration

--- appinventor/components/src/haus/orange/StreamLink/StreamLinkCamera.java
+++ appinventor/components/src/haus/orange/StreamLink/StreamLinkCamera.java
@@ -3,11 +3,15 @@ package haus.orange.StreamLink;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import com.google.appinventor.components.runtime.util.AsynchUtil;
 
 import android.content.Context;
 import android.hardware.Camera;
+import android.hardware.Camera.Size;
+import android.media.CamcorderProfile;
+import android.media.CameraProfile;
 import android.media.MediaRecorder;
 import android.os.ParcelFileDescriptor;
 import android.view.Surface;
@@ -16,142 +20,131 @@ import android.view.SurfaceView;
 
 @SuppressWarnings("deprecation")
 public class StreamLinkCamera extends SurfaceView implements SurfaceHolder.Callback {
-	
-	Camera camera;
+
 	SurfaceHolder holder;
 	Surface surface;
 	MediaRecorder recorder;
 	ByteArrayOutputStream dataStream;
+	boolean hasStarted;
 
-	public StreamLinkCamera(Context context, Camera camera) {
+	public StreamLinkCamera(Context context) {
 		super(context);
-		this.camera = camera;
 		holder = getHolder();
 		holder.addCallback(this);
-		recorder = new MediaRecorder();
 		dataStream = new ByteArrayOutputStream();
 	}
 
 	@Override
 	public void surfaceChanged(SurfaceHolder holder, int arg1, int arg2, int arg3) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void surfaceCreated(SurfaceHolder holder) {
-		
-		Camera.Parameters params = camera.getParameters();
-		
-		camera.setDisplayOrientation(90);
-		params.setRotation(90);
-		params.setPreviewFrameRate(30);
-		params.set("orientation", "portrait");
-		
-		camera.setParameters(params);
-	
-		
-		try {
-			camera.setPreviewDisplay(holder);
-			
-			camera.startPreview();
-			
-			surface = holder.getSurface();
-			
-			camera.unlock();
-			
-			startRecording();
-			
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
-		
+
+		surface = holder.getSurface();
+
 	}
-	
+
 	/**
 	 * Starts the live recording
 	 */
 	public void startRecording() {
-		
+
 		try {
+
+			final ParcelFileDescriptor[] descriptors = ParcelFileDescriptor.createPipe();
+			final ParcelFileDescriptor parcelRead = new ParcelFileDescriptor(descriptors[0]);
+			final ParcelFileDescriptor parcelWrite = new ParcelFileDescriptor(descriptors[1]);
+
+			final InputStream inputStream = new ParcelFileDescriptor.AutoCloseInputStream(parcelRead);
 			
-		    final ParcelFileDescriptor[] descriptors = ParcelFileDescriptor.createPipe();
-		    final ParcelFileDescriptor parcelRead = new ParcelFileDescriptor(descriptors[0]);
-		    final ParcelFileDescriptor parcelWrite = new ParcelFileDescriptor(descriptors[1]);
-	
-		    final InputStream inputStream = new ParcelFileDescriptor.AutoCloseInputStream(parcelRead);
-		    
-		    recorder.setPreviewDisplay(surface);
-		    recorder.setCamera(camera);
-		    
-		    //recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-		    recorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
-		    recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-		    //recorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
-		    recorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
-		    recorder.setVideoEncodingBitRate(2048000);
-		    recorder.setOrientationHint(90);
-		    recorder.setOutputFile(parcelWrite.getFileDescriptor());
-		    recorder.prepare();
-	
-		    recorder.start();
+			/**
+			 * 
+			 * Problem is unique to devices, not a single issue
+			 * 
+			 * 
+			 * PROBLEM BETWEEN HERE
+			 */
 			
+			recorder = new MediaRecorder();
+			
+			recorder.setPreviewDisplay(surface);
+			recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+			recorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+			recorder.setOutputFile(parcelWrite.getFileDescriptor());
+
+			CamcorderProfile profile = CamcorderProfile.get(CamcorderProfile.QUALITY_LOW);
+			recorder.setProfile(profile);
+			
+			/**
+			 * AND HERE
+			 */
+
+			if (!hasStarted) {
+				recorder.prepare();
+				recorder.start();
+				hasStarted = true;
+			}
+
 			AsynchUtil.runAsynchronously(new Runnable() {
-				
+
 				public void run() {
 					try {
-		
-					    int read;
-					    byte[] data = new byte[16384];
-		
-					    while ((read = inputStream.read(data, 0, data.length)) != -1) {
-					    	dataStream.write(data, 0, read);
-					    }
-		
-					    dataStream.flush();
-					    parcelWrite.close();
-					    inputStream.close();
-					}catch(IOException e) {
+
+						int read;
+						byte[] data = new byte[16384];
+
+						while ((read = inputStream.read(data, 0, data.length)) != -1) {
+							dataStream.write(data, 0, read);
+						}
+
+						dataStream.flush();
+						parcelWrite.close();
+						inputStream.close();
+					} catch (IOException e) {
 						e.printStackTrace();
 					}
-					
+
 				}
-				
+
 			});
-		} catch(IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Stops the live recording
 	 */
 	public void stopRecording() {
-		if(recorder != null) {
+		if (recorder != null) {
 			recorder.stop();
 			recorder.reset();
 			recorder.release();
+			hasStarted = false;
 		}
 	}
-	
+
 	/**
 	 * Grabs the most recent chunk from the recording
+	 * 
 	 * @return the most recent chunk
 	 */
 	public byte[] getData() {
-		
-		if(dataStream != null) {
+
+		if (dataStream != null) {
 			return dataStream.toByteArray();
 		}
-		
+
 		return null;
 	}
-	
 
 	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
